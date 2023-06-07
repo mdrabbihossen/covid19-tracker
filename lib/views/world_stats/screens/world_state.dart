6b014f7b67cdcdcd9b0818111daf5693dc146565
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-07 22:49:00 +0600

    world stats updated from API

--- lib/views/world_stats/screens/world_state.dart
+++ lib/views/world_stats/screens/world_state.dart
@@ -1,7 +1,10 @@
+import 'package:covid19_tracker/model/world_stats_model.dart';
+import 'package:covid19_tracker/services/stats_services.dart';
 import 'package:covid19_tracker/utils/app_colors.dart';
 import 'package:covid19_tracker/utils/widgets/reusable_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class WorldStats extends StatefulWidget {
   const WorldStats({Key? key}) : super(key: key);
@@ -38,52 +41,85 @@ class _WorldStatsState extends State<WorldStats> with TickerProviderStateMixin {
           child: Column(
             children: [
               SizedBox(height: size.height * 0.01),
-              PieChart(
-                dataMap: const {
-                  "Total Cases": 100,
-                  "Recovered": 50,
-                  "Deaths": 20,
-                },
-                animationDuration: const Duration(milliseconds: 1200),
-                chartType: ChartType.ring,
-                colorList: AppColors.colorList,
-                chartRadius: size.width / 3.2,
-                legendOptions: const LegendOptions(
-                  legendPosition: LegendPosition.left,
+              Expanded(
+                child: FutureBuilder(
+                  future: StatsServices().getWorldStats(),
+                  builder: (context, AsyncSnapshot<WorldStatsModel> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Expanded(
+                        flex: 1,
+                        child: SpinKitFadingCircle(
+                          color: AppColors.deepGreen,
+                          size: 50.0,
+                          controller: _controller,
+                        ),
+                      );
+                    } else {
+                      return Column(
+                        children: [
+                          PieChart(
+                            dataMap: {
+                              "Total Cases": snapshot.data!.cases!.toDouble(),
+                              "Recovered": snapshot.data!.recovered!.toDouble(),
+                              "Deaths": snapshot.data!.deaths!.toDouble(),
+                            },
+                            chartValuesOptions: const ChartValuesOptions(
+                              showChartValuesInPercentage: true,
+                              decimalPlaces: 1,
+                            ),
+                            animationDuration:
+                                const Duration(milliseconds: 1200),
+                            chartType: ChartType.ring,
+                            colorList: AppColors.colorList,
+                            chartRadius: size.width / 3.2,
+                            legendOptions: const LegendOptions(
+                              legendPosition: LegendPosition.left,
+                            ),
+                          ),
+                          SizedBox(height: size.height * 0.06),
+                          Card(
+                            child: Column(
+                              children: [
+                                Reusable(
+                                  title: 'Total',
+                                  trailing: snapshot.data!.cases.toString(),
+                                ),
+                                Reusable(title: 'Today Deaths', trailing: snapshot.data!.todayDeaths.toString()),
+                                Reusable(title: 'Critical', trailing: snapshot.data!.critical.toString()),
+                                Reusable(title: 'Affected Countries', trailing: snapshot.data!.affectedCountries.toString()),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: size.height * 0.06),
+                          ElevatedButton(
+                            onPressed: () {
+
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.deepGreen,
+                              minimumSize: Size(
+                                double.infinity,
+                                size.height * .07,
+                              ),
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: const Text(
+                              'Track Countries',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                  },
                 ),
-              ),
-              SizedBox(height: size.height * 0.06),
-              const Card(
-                  child: Column(
-                children: [
-                  Reusable(title: 'Total', trailing: "100"),
-                  Reusable(title: 'Total', trailing: "100"),
-                  Reusable(title: 'Total', trailing: "100"),
-                  Reusable(title: 'Total', trailing: "100"),
-                ],
-              )),
-              SizedBox(height: size.height * 0.06),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.deepGreen,
-                  minimumSize: Size(
-                    double.infinity,
-                    size.height * .07,
-                  ),
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text(
-                  'Track Countries',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
+              )
             ],
           ),
         ),
