a72c9eee6d53eea71503de4c432054d95abae758
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-07 22:19:29 +0600

    model and constants

---
+++ lib/utils/constants/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String baseUrl = "https://disease.sh/v3/covid-19/";
+  static const String worldStats = "${baseUrl}all";
+  static const String countries = "${baseUrl}countries";
+}
