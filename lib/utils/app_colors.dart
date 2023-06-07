cb2c1f66fe510e77ed5f57453d2fde10d93a009c
MD Rabbi Hossen <ariyanrabby94@gmail.com>
2023-06-07 14:23:50 +0600

    splash start

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color deepBlue=Color(0xff4285f4);
+  static const Color deepGreen=Color(0xff1aa260);
+  static const Color red=Color(0xffde5246);
+}
--- lib/views/splash/screen/splash_screen.dart
+++ lib/views/splash/screen/splash_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(vsync: this, duration: const Duration(seconds: 3))
+        ..repeat();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: animation,
+              builder: (context, child) {},
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
