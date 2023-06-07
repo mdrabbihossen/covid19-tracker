664a84132639c3fc5f229d4e415ea268eb90d47e
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-07 16:17:31 +0600

    world stats started:

--- lib/views/world_stats/screens/world_state.dart
+++ lib/views/world_stats/screens/world_state.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
 class WorldStats extends StatefulWidget {
   const WorldStats({Key? key}) : super(key: key);
 
@@ -6,9 +8,38 @@ class WorldStats extends StatefulWidget {
   State<WorldStats> createState() => _WorldStatsState();
 }
 
-class _WorldStatsState extends State<WorldStats> {
+class _WorldStatsState extends State<WorldStats> with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(vsync: this, duration: const Duration(seconds: 3))
+        ..repeat();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              SizedBox(height: size.height * 0.01),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
