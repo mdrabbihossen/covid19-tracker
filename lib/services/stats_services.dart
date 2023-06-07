11f7d567364533a0366bcade606591089f321e8a
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-07 23:17:38 +0600

    service

--- lib/services/stats_services.dart
+++ lib/services/stats_services.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:covid19_tracker/model/world_stats_model.dart';
 import 'package:covid19_tracker/utils/constants/app_constants.dart';
 import 'package:http/http.dart' as https;
 
 class StatsServices {
+  // get all stats
   Future<WorldStatsModel> getWorldStats() async {
     try {
       final response = await https.get(
@@ -10,6 +13,24 @@ class StatsServices {
       );
       if (response.statusCode == 200) {
         return worldStatsModelFromJson(response.body);
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // get all stats end
+
+  // get all countries
+  Future<List<dynamic>> getCountriesList() async {
+    try {
+      final response = await https.get(
+        Uri.parse(AppConstants.countries),
+      );
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
       } else {
         throw Exception('Failed to load data');
       }
