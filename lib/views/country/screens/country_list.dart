3c2d05f78ca51f87c25876724bcc3f90ffb9923d
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-07 23:21:29 +0600

    search country field

--- lib/views/country/screens/country_list.dart
+++ lib/views/country/screens/country_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class CountriesList extends StatefulWidget {
   const CountriesList({Key? key}) : super(key: key);
 
@@ -7,8 +8,36 @@ class CountriesList extends StatefulWidget {
 }
 
 class _CountriesListState extends State<CountriesList> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          // search country text field
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: searchController,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                hintText: "Search Country",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+          ),
+          // search country text field end
+        ],
+      )),
+    );
   }
 }
